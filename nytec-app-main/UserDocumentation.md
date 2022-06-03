79d34bf7d9d4ad40cfb4c8136514bbb587cdfb5a
DonaldNg123 <donald1234567890@gmail.com>
2022-06-03 15:32:03 -0400

    updated app

---
+++ nytec-app-main/UserDocumentation.md
@@ -0,0 +1,28 @@
+# User Documentation
+
+## Getting started
+
+When the app loads, you'll be presented with a login screen. Switch to sign up mode and enter your email and password.
+
+Upon signing up, you'll be presented with a screen to confirm your email. Press the left button to send the email containing the verfication link. Once you've clicked the link in your inbox, press the right button to proceed to the main page.
+
+You'll be presented with two tabs. You can change tabs at the bottom of the screen.
+
+## Main Page
+This tab contains all the different links you can click on. 
+
+## Accounts Page
+Here, you can see the email you're logged in with and can logout. 
+
+## FAQ
+1. I've forgotten my password. What do I do?
+
+At the bottom of the login page, you'll find an option to reset your password. Click the button, enter your email, and a link will be sent to your inbox to reset your password.
+
+2. I don't see the verification email. What do I do?
+
+Make sure to check your junk or spam inboxes! If you still don't receive an email, please contact me at sam4button@gmail.com and I'll work with you on the problem as quickly as I can.
+
+3. How do I receive notifications?
+
+Go to `Settings > Notifications > Expo Go > Allow Notifications`. Then logout and log back in.
---
+++ nytec-app-main/components/Account.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
+import Colors from "../constants/Colors";
+import { smallFontSize, mediumFontSize } from "../constants/Sizes";
+
+import Background from "./Background";
+import LogoutButton from "./LogoutButton";
+import PasswordChange from "./PasswordChange";
+
+import { useTranslation } from "react-i18next";
+import '../assets/i18n';
+
+
+
+export default function Account() {
+	//Used for Translating App
+	const {t, i18n} = useTranslation();
+	
+	const email = useSelector((state) => state.auth.email);
+
+	const [changing, setChanging] = useState(false);
+	return (
+		<Background>
+			<View style={styles.container}>
+				{!changing && (
+					<View>
+						<Text style={styles.loginText}>{t("e-mail1")}</Text>
+						<View style={styles.emailContainer}>
+							<Text style={styles.email}>{email}</Text>
+						</View>
+						<LogoutButton />
+					</View>
+				)}
+				<PasswordChange
+					email={email}
+					changing={changing}
+					setChanging={setChanging}
+				>
+					{t("changepassword")}
+				</PasswordChange>
+			</View>
+		</Background>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 15,
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	email: {
+		color: "white",
+		fontSize: smallFontSize,
+		fontWeight: "bold",
+		fontStyle: "italic",
+	},
+	emailContainer: {
+		paddingVertical: 20,
+		paddingHorizontal: 30,
+		borderRadius: 15,
+		backgroundColor: Colors.complement,
+		marginBottom: "8%",
+		shadowOffset: { width: 0, height: 3 },
+		shadowRadius: 6,
+		shadowOpacity: 0.5,
+		elevation: 10,
+		shadowColor: "white",
+	},
+	loginText: {
+		marginTop: 15,
+		marginBottom: 20,
+		color: "white",
+		fontSize: mediumFontSize,
+		textAlign: "center",
+	},
+});
